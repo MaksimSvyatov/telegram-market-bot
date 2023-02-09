726bd6526af539f7799289e678f022b48ec844c5
Svyatov Maksim <masvjatov@gmail.com>
2023-02-09 17:07:48 +0400

    Add inline button for additional info

--- handlers/client_part.py
+++ handlers/client_part.py
@@ -4,6 +4,7 @@ from create_bot import dp, bot
 # from keyboards import client_part_kb
 # from aiogram.types import ReplyKeyboardRemove
 import keyboards.client_part_kb as nav
+import asyncio
 
 # import handlers.admin_part as states
 import requests
@@ -15,78 +16,115 @@ async def command_start(message: types.Message):
     try:
         await bot.send_message(
             message.from_user.id,
-            "Привет. Я market-bot). Выберите о чём ты хотел бы получить данные",
-            reply_markup=nav.main_menu_kb_client_part,
+            "Привет. Я market-bot). Выберите о чём ты хотел бы получить данные"
+            # , reply_markup=nav.main_menu_kb_client_part
         )
+        
         # await message.delete()
     except:
         await message.reply(
             "Напишите боту в ЛС, напишите ему: \nhttps://t.me/Investment_FollowUp_bot"
         )
 
+ticker = ''
 
 # @dp.message_handler()
 async def bot_message(message: types.Message):
     await bot.send_message(message.from_user.id, message.text)
-    if message.text == "Акции":
-        await bot.send_message(
-            message.from_user.id,
-            "Укажите тикер акции и нажмите кнопку",
-            reply_markup=nav.share_current_price_menu_kb_client_part,
-        )
+    # if message.text == "Акции":
+    #     await bot.send_message(
+    #         message.from_user.id,
+    #         "Укажите тикер акции и нажмите кнопку"
+    #         # ,
+    #         # reply_markup=nav.share_current_price_menu_kb_client_part
+    #     )
         # try:
     if len(message.text) == 4:
-        data = requests.get(
+        
+            # ticker = message.text
+            data = requests.get(
             f"https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities/{message.text}.json"
-        ).text
-        data = json.loads(data)
-        share_current_price_index = data["marketdata"]["columns"].index("LCURRENTPRICE")
-        data_list = data["marketdata"]["data"][0]
-        share_current_price = data_list[share_current_price_index]
-        await bot.send_message(
-            message.from_user.id, f"Current share price is {share_current_price}"
-        )
+            ).text
+            data = json.loads(data)
+            share_current_price_index = data["marketdata"]["columns"].index("LCURRENTPRICE")
+            data_list = data["marketdata"]["data"][0]
+            share_current_price = data_list[share_current_price_index]
+            await bot.send_message(
+                message.from_user.id, f"Current share price is {share_current_price}",
+                reply_markup=nav.main_menu_with_addinfo_button
+            )
+            print("From 4 length")
+            
+
+
+        # ticker = str(get())
+        # ticker.close()
+            
+            
+    
     # except:
     #     # if len(message.text) < 4 or len(message.text) > 4:
     #         await bot.send_message(message.from_user.id,
     #             'Ooops( Something goes wrong! Try another ticket, or go to "Облигации"')
     # await states.FSMClient.shares.set()
-    elif message.text == "Облигации":
-        await bot.send_message(
-            message.from_user.id,
-            f"Укажите тикер облигации и нажмите кнопку",
-            reply_markup=nav.bond_current_price_menu_kb_client_part,
-        )
+    # elif message.text == "Облигации":
+    #     await bot.send_message(
+    #         message.from_user.id,
+    #         f"Укажите тикер облигации и нажмите кнопку",
+    #         reply_markup=nav.bond_current_price_menu_kb_client_part,
+    #     )
         # await states.FSMClient.bonds.set()
-    if message.text[0] == "S":
+    elif message.text.startswith("SU"):
         data = requests.get(
             f"https://iss.moex.com/iss/engines/stock/markets/bonds/boards/TQOB/securities/{message.text}.json"
         ).text
         data = json.loads(data)
+        ticker = message.text
         bond_current_price_index = data["marketdata"]["columns"].index("LCURRENTPRICE")
         data_list = data["marketdata"]["data"][0]
         bond_current_price = data_list[bond_current_price_index]
         await bot.send_message(
-            message.from_user.id, f"Current bond price is {bond_current_price}"
+            message.from_user.id, f"Current bond price is {bond_current_price}",
+            reply_markup=nav.main_menu_with_addinfo_button
         )
-    else:
+        print("From SU")
+        
+    elif message.text.startswith("RU"):
         data = requests.get(
             f"https://iss.moex.com/iss/engines/stock/markets/bonds/boards/TQCB/securities/{message.text}.json"
         ).text
         data = json.loads(data)
+        ticker = message.text
         bond_current_price_index = data["marketdata"]["columns"].index("LCURRENTPRICE")
         data_list = data["marketdata"]["data"][0]
         bond_current_price = data_list[bond_current_price_index]
         await bot.send_message(
-            message.from_user.id, f"Current bond price is {bond_current_price}"
+            message.from_user.id, f"Current bond price is {bond_current_price}",
+            reply_markup=nav.main_menu_with_addinfo_button
         )
-    if message.text == "Главное меню":
-        await bot.send_message(
-            message.from_user.id,
-            "Выберите о чём бы Вы хотели получить данные",
-            # input_field_placeholder = "Введите тикер инструмента",
-            reply_markup=nav.main_menu_kb_client_part,
-        )
+        print("From RU")
+@dp.callback_query_handler(text='get_add_info')
+async def get_add_info(callback : types.CallbackQuery):
+                await callback.answer('Addinfo pressed')   
+
+        # data = requests.get(
+        #     f"https://iss.moex.com/iss/engines/stock/markets/bonds/boards/TQCB/securities/{message.text}.json"
+        # ).text
+        # data = json.loads(data)
+        # bond_current_price_index = data["marketdata"]["columns"].index("LCURRENTPRICE")
+        # data_list = data["marketdata"]["data"][0]
+        # bond_current_price = data_list[bond_current_price_index]
+        # await bot.send_message(
+        #     message.from_user.id, f"Current bond price is {bond_current_price}",
+        #     reply_markup=nav.main_menu_with_addinfo_button
+        # )
+    # if message.text == "Главное меню":
+    #     await bot.send_message(
+    #         message.from_user.id,
+    #         "Выберите о чём бы Вы хотели получить данные",
+    #         # input_field_placeholder = "Введите тикер инструмента",
+    #         reply_markup=nav.main_menu_kb_client_part
+    #     )
 
 
 # @dp.message_handler(commands=['Акции'])
--- keyboards/client_part_kb.py
+++ keyboards/client_part_kb.py
@@ -1,31 +1,34 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton#, ReplyKeyboardRemove
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 
 # --- Main Menu ----
-shares_button = KeyboardButton("Акции")
-bonds_button = KeyboardButton("Облигации")
+# shares_button = KeyboardButton("Акции")
+# bonds_button = KeyboardButton("Облигации")
 main_menu_button = KeyboardButton("Главное меню")
-additional_info_button = KeyboardButton("Получить дополнительную информацию")
+additional_info_button = InlineKeyboardButton(text="Получить дополнительную информацию", callback_data="get_add_info")
 # share_client_number = KeyboardButton('Поделиться номером', request_contact=True)
 # share_client_location = KeyboardButton('Пделиться местоположением', request_location=True)
 
-main_menu_kb_client_part = ReplyKeyboardMarkup(resize_keyboard=True)
+# main_menu_kb_client_part = ReplyKeyboardMarkup(resize_keyboard=True)
 
-main_menu_kb_client_part.add(shares_button).add(bonds_button)  # .row(share_client_number, share_client_number)
+main_menu_with_addinfo_button = InlineKeyboardMarkup(resize_keyboard=True)
+main_menu_with_addinfo_button.add(additional_info_button)
+# main_menu_kb_client_part.add(shares_button).add(bonds_button)  # .row(share_client_number, share_client_number)
 # main_menu_kb_client_part.add(shares_button).insert(bonds_button)
 # main_menu_kb_client_part.row(shares_button, bonds_button)
 
 # --- Shares Menu ---
 
 # current_price_share = KeyboardButton('Узнать стоимость акции')
-share_current_price_menu_kb_client_part = ReplyKeyboardMarkup(resize_keyboard=True)
+# share_current_price_menu_kb_client_part = ReplyKeyboardMarkup(resize_keyboard=True)
 
-share_current_price_menu_kb_client_part.add(additional_info_button).add(main_menu_button)
+# share_current_price_menu_kb_client_part.add(additional_info_button)#.add(main_menu_button)
 
 # --- Bonds Menu ---
 
 # current_price_share = KeyboardButton('Узнать стоимость облигации')
-bond_current_price_menu_kb_client_part = ReplyKeyboardMarkup(resize_keyboard=True)
+# bond_current_price_menu_kb_client_part = ReplyKeyboardMarkup(resize_keyboard=True)
 
-bond_current_price_menu_kb_client_part.add(additional_info_button).add(main_menu_button)
+# bond_current_price_menu_kb_client_part.add(additional_info_button).add(main_menu_button)
