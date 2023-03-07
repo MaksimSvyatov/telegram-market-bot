d78875ca21c81e08f6fa3b1c31df11545938fd52
MaksimSvyatov <masvjatov@gmail.com>
2023-03-07 13:29:48 +0400

    Prepare to deploy

--- create_bot.py
+++ create_bot.py
@@ -5,6 +5,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 
 storage=MemoryStorage()
-
-bot = Bot(token=os.getenv('TOKEN'))
+TOKEN='5806158899:AAEF5GdihaO4lhBksUT7NI7D42ErlzVZ3io'
+print(type(TOKEN))
+bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=storage)
