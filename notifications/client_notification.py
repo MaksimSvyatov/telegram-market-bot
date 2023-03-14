430c57e2ba8aa3e9a4d6dfe494f5c3a6e20eb81f
Svyatov Maksim <masvjatov@gmail.com>
2023-03-14 16:06:24 +0400

    Add check daily notificaton

--- notifications/client_notification.py
+++ notifications/client_notification.py
@@ -4,7 +4,7 @@ def greeting ():
     print('This is notification')
 
 def get_notification():
-    schedule.every().day.at('16:00').do(greeting)
+    schedule.every().day.at('16:10').do(greeting)
     
     while True:
         schedule.run_pending()
