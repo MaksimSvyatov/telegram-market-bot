b78c8af77b9552cef1babcc9077975d7a146b0cd
MaksimSvyatov <masvjatov@gmail.com>
2023-03-23 15:36:54 +0400

    Version 1.0

--- data_base/sqlite_db.py
+++ data_base/sqlite_db.py
@@ -1,5 +1,4 @@
 import sqlite3 as sq
-from create_bot import dp, bot
 
 def sql_start():
     global base, cur
@@ -7,16 +6,30 @@ def sql_start():
     cur = base.cursor()
     if base:
         print('Data base connected OK!')
-    base.execute('CREATE TABLE IF NOT EXISTS {}(tickers TEXT, periodicity TEXT, time TEXT)'.format('data'))
+    base.execute('CREATE TABLE IF NOT EXISTS {}(tickers TEXT, periodicity TEXT, time TEXT, id TEXT)'.format('data'))
     base.commit()
         
 async def sql_add_command(state):
     async with state.proxy() as data:
-        cur.execute('INSERT INTO data VALUES(?,?,?)', tuple(data.values()))
+        cur.execute('INSERT INTO data VALUES(?,?,?,?)', tuple(data.values()))
         base.commit()
         
-async def sql_read(message):
+async def sql_read():
     for ret in cur.execute('SELECT * FROM data').fetchall():
-        await bot.send_message(message.from_user.id, f'Tickers: {ret[0]}, periodicity: {ret[1]}, time: {ret[2]}') #\nType: {ret[2]}')
-    # r = cur.execute('SELECT * FROM data').fetchall()
-    # print(r)
+        # await bot.send_message(message.from_user.id, f'Tickers: {ret[0]}, periodicity: {ret[1]}, time: {ret[2]}') #\nType: {ret[2]}')
+        data_base_list = list(cur.execute('SELECT * FROM data').fetchall())
+        return data_base_list
+        print(type(data_base_list)) 
+
+# async def fetch_one_item():
+#     # for ret in cur.execute('SELECT tickers FROM data WHERE id = 99117096').fetchone():
+#         tickers = list(cur.execute('SELECT tickers FROM data WHERE id = 99117096').fetchone())
+#         print(tickers)
+#         return tickers
+
+# def sql_read1():
+#     for ret in cur.execute('SELECT * FROM data').fetchall():
+#         # await bot.send_message(message.from_user.id, f'Tickers: {ret[0]}, periodicity: {ret[1]}, time: {ret[2]}') #\nType: {ret[2]}')
+#         data_base_list1 = list(cur.execute('SELECT * FROM data').fetchall())
+#         return data_base_list1
+#         print(type(data_base_list)) 
